23110eb01c5457c0df925ac5cd6d5f5d0f834330
Søren Louv-Jansen <sorenlouv@gmail.com>
2016-01-08 02:16:46 +0100

    Added momentjs and chartService

--- src/browser/components/User.jsx
+++ src/browser/components/User.jsx
@@ -1,161 +1,26 @@
-var _ = require('lodash');
 var React = require('react');
 var Highcharts = require('react-highcharts/bundle/highcharts');
 var userService = require('../services/user');
-
-function getLabels(timestamps) {
-    return _(timestamps)
-    .reduce(function(memo, timestamp, i) {
-        var isLast = i === (timestamps.length - 1);
-        if (isLast) {
-            return memo;
-        }
-
-        var nextTimestamp = timestamps[i + 1];
-        var durationHours = (nextTimestamp - timestamp) / 1000 / 60 / 60;
-        var hour = new Date(timestamp).getHours();
-
-        if(durationHours > 2 && durationHours < 14 && (hour > 21 || hour < 6)) {
-            memo.push({
-                from: timestamp,
-                to: nextTimestamp
-            });
-        }
-
-        return memo;
-    }, [])
-    .map(function (interval, i, intervals) {
-        var MIN_DAY_LENGTH = 12 * 3600 * 1000;
-        var isLast = i === (intervals.length - 1);
-        if (isLast || interval.delete) {
-            return interval;
-        }
-
-        var nextInterval = intervals[i + 1];
-        var awakeDuration = nextInterval.from - interval.to;
-        if(awakeDuration < MIN_DAY_LENGTH) {
-            var nextIntervalDuration = (nextInterval.to - nextInterval.from);
-            var intervalDuration = (interval.to - interval.from);
-            if(awakeDuration < 15 * 60 * 1000 && (nextIntervalDuration + intervalDuration) < 14 * 3600 * 1000) {
-                interval.delete = true;
-                nextInterval.from = interval.from;
-            }else {
-                var nextIsLarger = nextIntervalDuration > intervalDuration;
-                if(nextIsLarger) {
-                    interval.delete = true;
-                } else {
-                    nextInterval.delete = true;
-                }
-            }
-        }
-
-        return interval;
-    }, [])
-    .filter(function (interval) {
-        return !interval.delete;
-    })
-    .map(function (sleep) {
-        var yesterday = getYesterday(sleep.to);
-        var duration = (sleep.to - sleep.from) / 1000 / 60 / 60;
-        return {
-          color: '#dfe3ee',
-          from: sleep.from,
-          to: sleep.to,
-          label: {
-            text: '<strong>' + yesterday + '</strong><br>' + duration.toFixed(1) + ' hrs.<br> of sleep',
-            fontWeight: 'bold',
-            verticalAlign: 'middle',
-            y: 0
-          }
-        }
-    });
-}
-
-function getYesterday(timestamp) {
-    var days = ['Sun','Mon','Tue','Wed','Thur','Fri','Sat'];
-    var d = new Date(timestamp);
-    d.setDate(d.getDate() - 1)
-    var day = d.getDay();
-    return days[day];
-}
-
-function getConfig(timestamps) {
-    if(_.isEmpty(timestamps)) {
-        return {};
-    }
-
-    var labels = getLabels(timestamps);
-    var data = timestamps.map(function(timestamp){
-        return [timestamp, 1];
-    });
-
-    return {
-        plotOptions: {
-            series: {
-                animation: false,
-            },
-        },
-        legend: {
-            enabled: false,
-        },
-        chart: {
-            height: 300,
-            renderTo: 'container',
-            type: 'column',
-        },
-        title: {
-            text: 'Availability',
-        },
-        xAxis: {
-            type: 'datetime',
-            dateTimeLabelFormats: {
-                month: '%e. %b',
-                year: '%b',
-            },
-            plotBands: labels
-        },
-        yAxis: {
-            max: 1,
-            min: 0,
-            minTickInterval: 1,
-            labels: {
-                enabled: false,
-            },
-            title: {
-                text: '',
-            },
-        },
-        tooltip: {
-            formatter: function() {
-                function pad(n) { return ('0' + n).slice(-2); }
-                var days = ['Sun','Mon','Tue','Wed','Thur','Fri','Sat'];
-                var d = new Date(this.x);
-                var weekDay = days[d.getDay()];
-                var hours = pad(d.getHours());
-                var minutes = pad(d.getMinutes());
-                return weekDay + ' ' + hours + ':' + minutes;
-            },
-        },
-        series: [{
-            color: '#3b5998',
-            name: '-',
-            data: data,
-        }]
-    };
-}
+var chartService = require('../services/chart');
+var moment = require('moment');
 
 module.exports = React.createClass({
     getUser(userId) {
         var _this = this;
         userService.getActivity(userId)
-            .then(function(userActivity) {
-                _this.setState({ userActivity: userActivity, userId: userId });
+            .then(function(timestamps) {
+                _this.setState({
+                    userId: userId,
+                    config: chartService.getConfig(timestamps),
+                    intervals: chartService.getSleepIntervals(timestamps)
+                });
             })
             .done();
     },
     getInitialState() {
         return {
-            userActivity: [],
+            config: {},
+            intervals: []
         };
     },
     componentWillReceiveProps(props) {
@@ -165,6 +30,31 @@ module.exports = React.createClass({
         this.getUser(this.props.params.userId);
     },
     render() {
-        return <Highcharts config = {getConfig(this.state.userActivity)}></Highcharts>
+        var intervalNodes = this.state.intervals.map(function (interval, i) {
+            return (
+                <tr key={i}>
+                    <td>{moment(interval.key).format('dddd, MMM DD')}</td>
+                    <td>{moment(interval.from).format('HH:mm')}-{moment(interval.to).format('HH:mm')}</td>
+                    <td>{interval.duration} hours</td>
+                </tr>
+            );
+        });
+
+        return (
+            <div>
+                <Highcharts config={this.state.config}></Highcharts>
+
+                <table className="interval-overview">
+                    <thead>
+                        <tr>
+                            <td></td>
+                            <td>Period</td>
+                            <td>Duration</td>
+                        </tr>
+                    </thead>
+                    <tbody>{intervalNodes}</tbody>
+                </table>
+            </div>
+        );
     }
 });
---
+++ src/browser/services/chart.js
@@ -0,0 +1,189 @@
+var _ = require('lodash');
+var chartService = {};
+
+function cloneDate(d) {
+    return new Date(d.getTime());
+}
+
+function getSleepScore(timestampStart, timestampEnd) {
+    var dateStart = new Date(timestampStart);
+    var dateEnd = new Date(timestampEnd);
+
+    var standardStart = new Date(cloneDate(dateEnd).setDate(dateEnd.getDate() - 1)).setHours(22);
+    var standardEnd = cloneDate(dateEnd).setHours(8);
+
+    var startDeviation = Math.pow(toHours(Math.abs(standardStart - dateStart)), 2);
+    var endDeviation = Math.pow(toHours(Math.abs(standardEnd - dateEnd)), 3);
+
+    var STANDARD_SLEEP_DURATION = 8;
+    var duration = toHours(timestampEnd - timestampStart);
+    var durationDeviation = Math.pow(Math.abs(duration - STANDARD_SLEEP_DURATION), 3);
+
+    return startDeviation + endDeviation + durationDeviation;
+}
+
+function fromMinutes(minutes) {
+    return 1000 * 60 * minutes;
+}
+
+function toHours(milliseconds) {
+    return (milliseconds / (1000 * 60 * 60)).toFixed(1);
+}
+
+function getYesterday(timestamp) {
+    var days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thur', 'Fri', 'Sat'];
+    var d = new Date(timestamp);
+    d.setDate(d.getDate() - 1);
+    var day = d.getDay();
+    return days[day];
+}
+
+chartService.getSleepIntervals = function(timestamps) {
+    if (!timestamps) {
+        return [];
+    }
+
+    var a = _.chain(timestamps).reduce(function(memo, timestamp, i) {
+            var timestampStart = timestamp;
+            var timestampEnd = timestamps[i + 1];
+            if (!timestampEnd) {
+                return memo;
+            }
+
+            memo.push({
+                from: timestampStart,
+                to: timestampEnd
+            });
+
+            return memo;
+        }, [])
+        .reduce(function(memo, interval, i, intervals) {
+            var nextInterval = intervals[i + 1];
+            if (!nextInterval) {
+                return memo;
+            }
+
+            var timeBeetweenIntervals = nextInterval.from - interval.to;
+            if (timeBeetweenIntervals < fromMinutes(15) && interval.combineCount < 3) {
+                memo.push({
+                    from: interval.from,
+                    to: nextInterval.to,
+                    combineCount: _.add(interval.combineCount, 1)
+                });
+            }
+
+            memo.push(interval);
+            return memo;
+        }, [])
+        .map(function(interval, i, intervals) {
+            interval.score = getSleepScore(interval.from, interval.to);
+            interval.duration = toHours(interval.to - interval.from);
+            return interval;
+        })
+        .sortByOrder('score', 'asc')
+        .reduce(function(memo, interval, i) {
+            var d = new Date(interval.to);
+            var key = new Date(d.getFullYear(), d.getMonth(), d.getDate() - 1).getTime();
+
+            if (!_.some(memo, {key: key})) {
+                interval.key = key;
+                memo.push(interval);
+            }
+
+            return memo;
+        }, [])
+        .filter(function(interval) {
+            return interval.score < 200;
+        })
+        .sortBy('key')
+        .value();
+
+    return a;
+};
+
+function getPlotBands(timestamps) {
+    return chartService.getSleepIntervals(timestamps)
+        .map(function(interval) {
+            var yesterday = getYesterday(interval.to);
+            var duration = interval.to - interval.from;
+            return {
+                color: '#dfe3ee',
+                from: interval.from,
+                to: interval.to,
+                label: {
+                    text: '<strong>' + yesterday + '</strong><br>' + toHours(duration) + ' hrs.<br> of sleep',
+                    fontWeight: 'bold',
+                    verticalAlign: 'middle',
+                    y: 0
+                }
+            };
+        });
+}
+
+chartService.getConfig = function(timestamps) {
+    if (_.isEmpty(timestamps)) {
+        return {};
+    }
+
+    var data = timestamps.map(function(timestamp) {
+        return [timestamp, 1];
+    });
+
+    return {
+        plotOptions: {
+            series: {
+                animation: false,
+            },
+        },
+        legend: {
+            enabled: false,
+        },
+        chart: {
+            height: 300,
+            renderTo: 'container',
+            type: 'column',
+        },
+        title: {
+            text: 'Availability',
+        },
+        xAxis: {
+            type: 'datetime',
+            dateTimeLabelFormats: {
+                month: '%e. %b',
+                year: '%b',
+            },
+            plotBands: getPlotBands(timestamps)
+        },
+        yAxis: {
+            max: 1,
+            min: 0,
+            minTickInterval: 1,
+            labels: {
+                enabled: false,
+            },
+            title: {
+                text: '',
+            },
+        },
+        tooltip: {
+            formatter: function() {
+                function pad(n) {
+                    return ('0' + n).slice(-2);
+                }
+                var days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thur', 'Fri', 'Sat'];
+                var d = new Date(this.x);
+                var weekDay = days[d.getDay()];
+                var hours = pad(d.getHours());
+                var minutes = pad(d.getMinutes());
+                return weekDay + ' ' + hours + ':' + minutes;
+            },
+        },
+        series: [{
+            color: '#3b5998',
+            name: '-',
+            data: data,
+        }]
+    };
+};
+
+module.exports = chartService;
