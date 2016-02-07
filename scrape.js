dfeba07f735643b4c938089afcae504bc575bfab
Søren Louv-Jansen <sorenlouv@gmail.com>
2016-02-08 00:36:51 +0100

    Catch exceptions in scraper

--- scrape.js
+++ scrape.js
@@ -3,19 +3,28 @@ var config = require('config');
 var fbCookie = config.get('fbCookie');
 var fbSleep = require('fb-sleep');
 var userService = require('./src/server/services/user');
-var POLLING_INTERVAL = 1000 * 60 * 10;
 
-function getAndSaveUsers(config, timeSinceLastCheck) {
-    fbSleep.getRecentlyActiveUsers(config, timeSinceLastCheck)
+function getRandomDelay() {
+    var POLLING_INTERVAL = 1000 * 60;
+    var delay = _.random(POLLING_INTERVAL * 0.9, POLLING_INTERVAL);
+    return delay;
+}
+
+function getAndSaveUsers(config, since) {
+    fbSleep.getRecentlyActiveUsers(config, since)
         .then(function(users) {
             console.log(new Date().toLocaleString(), ' - ', users.length, 'active users');
             return userService.saveUsers(users);
         })
+        .catch(function(err) {
+            console.error(new Date().toLocaleString(),
+                ' - Could not get users:', err.message, err.statusCode);
+        })
         .then(function() {
-            var delay = _.random(POLLING_INTERVAL * 0.9, POLLING_INTERVAL);
-            setTimeout(getAndSaveUsers.bind(null, config, delay), delay);
+            var since = Date.now();
+            setTimeout(getAndSaveUsers, getRandomDelay(), config, since);
         })
         .done();
 }
 
-getAndSaveUsers(fbCookie, POLLING_INTERVAL);
+getAndSaveUsers(fbCookie, Date.now() - 1000 * 60);
